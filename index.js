2cc73b1cc5628f1cf0da35604a1d00ae23ebf339
Niels Segers <segers.n@hotmail.com>
2018-05-02 16:34:08 +0200

    add several security and error checks

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 const program = require('commander');
-const data = require('./lib/data.js');
 const package = require('./package.json');
 const colors = require('colors/safe');
 const _ = require('lodash');
+const fs = require('fs');
+
+const data = require('./lib/data.js');
+const util = require('./lib/util.js');
 
 let file;
 const stdin = process.stdin;
@@ -12,9 +15,14 @@ const input = [];
 program.version(package.version)
     .option('-c, --clean', 'only output the lines that are different')
     .parse(process.argv);
-    
-const exec = (content, clean) => {
-    data.grabDefinitions(content, clean)
+
+const exec = (input, clean) => {
+    if (typeof clean === 'undefined' || !clean) console.log('- Grabbing the container definitions');
+    if (input.length === 0) { // when stdin has no container definitions
+        console.error(colors.red("    × No container definitions found in plan"));
+        process.exit();
+    }
+    data.grabDefinitions(input, clean)
         .then(data.prepareForAction)
         .then(data.processDefinitions)
         .catch(err => console.error(colors.red('    × Grabbing unsuccessful')));
@@ -24,18 +32,18 @@ if (!process.stdin.isTTY) {
     stdin.resume();
     stdin.setEncoding('utf8');
     stdin.on('data', content => {
-        const grep = (string, patternToSearch) => {
-            var regexPatternToSearch = new RegExp("^.*(" + patternToSearch + ").*$", "mg");
-            return string.match(regexPatternToSearch);
-        }
-        input.push(grep(content, 'container_definitions:'));
+        input.push(util.grep(content, 'container_definitions:'));
     });
     stdin.on('end', () => {
+        if (program.args.length !== 0) {
+            console.error(colors.red("ERR: received both stdin and file"));
+            process.exit();
+        }
         exec(_.compact(input).join("\n"), program.clean);
     });
 } else {
-    if (program.args.length === 0) {
-        console.error('ERR: no file specified');
+    if (program.args.length === 0 || !fs.existsSync(program.args[0])) {
+        console.error(colors.red("ERR: no file specified or doesn't exist"));
         process.exit();
     }
     exec(program.args[0], program.clean);
