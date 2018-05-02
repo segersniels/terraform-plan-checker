18ce3c9e91e6c0dd38f0e14813edfd67e99c7668
Niels Segers <segers.n@hotmail.com>
2018-05-02 15:22:37 +0200

    allow user to pipe terraform plan show to stdin

--- index.js
+++ index.js
@@ -2,18 +2,41 @@
 const program = require('commander');
 const data = require('./lib/data.js');
 const package = require('./package.json');
+const colors = require('colors/safe');
+const _ = require('lodash');
 
-program
-    .version(package.version)
+let file;
+const stdin = process.stdin;
+const input = [];
+
+program.version(package.version)
     .option('-c, --clean', 'only output the lines that are different')
     .parse(process.argv);
-
-if (program.args.length > 0) {
-    const file = process.argv[2];
-    data.grabDefinitions(file, program.clean)
+    
+const exec = (content, clean) => {
+    data.grabDefinitions(content, clean)
         .then(data.prepareForAction)
         .then(data.processDefinitions)
-        .catch(err => console.error(err));
+        .catch(err => console.error(colors.red('    × Grabbing unsuccessful')));
+}
+
+if (!process.stdin.isTTY) {
+    stdin.resume();
+    stdin.setEncoding('utf8');
+    stdin.on('data', content => {
+        const grep = (string, patternToSearch) => {
+            var regexPatternToSearch = new RegExp("^.*(" + patternToSearch + ").*$", "mg");
+            return string.match(regexPatternToSearch);
+        }
+        input.push(grep(content, 'container_definitions:'));
+    });
+    stdin.on('end', () => {
+        exec(_.compact(input).join("\n"), program.clean);
+    });
 } else {
-    program.help();
+    if (program.args.length === 0) {
+        console.error('ERR: no file specified');
+        process.exit();
+    }
+    exec(program.args[0], program.clean);
 }
